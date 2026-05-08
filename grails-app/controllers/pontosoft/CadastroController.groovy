0264880625ef52c606224662b04f10ac847b6273
Arthur Emmert <arthuremmert@insoft4.com>
2026-05-08 09:52:46 -0300

    Verificação de CPF duplicado

--- grails-app/controllers/pontosoft/CadastroController.groovy
+++ grails-app/controllers/pontosoft/CadastroController.groovy
@@ -17,9 +17,19 @@ class CadastroController {
         colaborador.email = params.email
         colaborador.tipoColab = params.tipoColab
         
+        def cpfExistente = Colaborador.findByCpf(params.cpf)
+        
+        if (cpfExistente) {
+            flash.erro = "Já existe um colaborador cadastrado com este CPF."
+            return render(view: "index", model: [
+                    colaborador: params
+            ])
+        }
+        
         if (colaborador.save(flush: true)) {
             flash.sucesso = "Colaborador cadastrado com sucesso!"
             redirect(action: "index")
+            
         } else {
             flash.erro = "Erro ao cadastrar colaborador."
             render(view: "index", model: [colaborador: colaborador])
